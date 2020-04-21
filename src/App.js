1c133e954e6a1d95928fddf1e71d30acfa3c7e45
Ayana Osawa <ayana.osawa@gmail.com>
2020-04-21 16:35:02 -0400

    Update design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react'
 // import Chart1 from './Chart1'
 import Chart2 from './Chart2'
+import './App.css'
+
+const data1 = [12, 26, 6, 25, 35, 10, 20]
+const data2 = [15, 10, 20]
+const data3 = [12, 6, 6, 25, 19]
+
+const w = 500
+const h = 300
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className="container">
+			<h1>My D3 Chart</h1>
+			<div className="chart">
 				{/* <Chart1 /> */}
-				<Chart2 />
-			</header>
+				<div className="chart__item">
+					<Chart2 data={data1} w={w} h={h} color="#ffd460" />
+				</div>
+				<div className="chart__item">
+					<Chart2 data={data2} w={w} h={h} color="#f07b3f" />
+				</div>
+				<div className="chart__item">
+					<Chart2 data={data3} w={w} h={h} color="#ea5455" />
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/Chart2.js
+++ src/Chart2.js
@@ -1,32 +1,30 @@
 import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 
-const BarChart = () => {
-	const myRef = useRef()
+const BarChart = (props) => {
+	const { data, w, h, color } = props
+
+	const chartRef = useRef()
 	useEffect(() => {
 		drawChart()
 	})
 
 	function drawChart() {
-		const data = [12, 36, 6, 25, 35, 10, 20]
-		const w = 500
-		const h = 400
-
-		const accessToRef = d3.select(myRef.current).append('svg').attr('width', w).attr('height', h).style('background-color', '#eee').style('padding', 10).style('margin-left', 50)
+		const accessToRef = d3.select(chartRef.current).append('svg').attr('width', w).attr('height', h).style('background', '#fff').style('padding', '20')
 
 		accessToRef
 			.selectAll('rect')
 			.data(data)
 			.enter()
 			.append('rect')
-			.attr('x', (d, i) => i * 70)
+			.attr('x', (d, i) => i * 65)
 			.attr('y', (d, i) => h - 10 * d)
-			.attr('width', 60)
+			.attr('width', 50)
 			.attr('height', (d, i) => 10 * d)
-			.attr('fill', 'tomato')
+			.attr('fill', color)
 	}
 
-	return <div ref={myRef}></div>
+	return <div ref={chartRef}></div>
 }
 
 // class BarChart extends Component {
