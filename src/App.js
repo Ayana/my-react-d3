6da608c4cf4b65be85ac1e0d30b1b928179d3434
Ayana Osawa <ayana.osawa@gmail.com>
2020-04-20 22:27:54 -0400

    Add chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import * as d3 from 'd3';
+import React from 'react'
+// import Chart1 from './Chart1'
+import Chart2 from './Chart2'
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <Chart1 /> */}
+				<Chart2 />
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/Chart2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+
+class BarChart extends Component {
+	constructor(props) {
+		super(props)
+		this.myRef = React.createRef()
+	}
+
+	componentDidMount() {
+		this.drawChart()
+	}
+
+	drawChart() {
+		const data = [12, 36, 6, 25, 35, 10, 20]
+		const w = 500
+		const h = 400
+
+		const accessToRef = d3.select(this.myRef.current).append('svg').attr('width', w).attr('height', h).style('background-color', '#eee').style('padding', 10).style('margin-left', 50)
+
+		accessToRef
+			.selectAll('rect')
+			.data(data)
+			.enter()
+			.append('rect')
+			.attr('x', (d, i) => i * 70)
+			.attr('y', (d, i) => h - 10 * d)
+			.attr('width', 60)
+			.attr('height', (d, i) => 10 * d)
+			.attr('fill', 'tomato')
+	}
+
+	render() {
+		return <div ref={this.myRef}></div>
+	}
+}
+
+export default BarChart
